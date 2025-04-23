e94aeb4d35def751ad8a5df58b63c2259692677a
harshchaudhary50 <chaudhariharsh960@gmail.com>
2025-04-23 12:36:38 +0530

    C_header

---
+++ harsh.c
@@ -0,0 +1,8 @@
+// main.c
+#include "harshh.h"
+
+int main() {
+    Hello(1);  // Output: Hello
+    Hello(0);  // Output: bye
+    return 0;
+}
---
+++ harshh.h
@@ -0,0 +1,15 @@
+// hello.h
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <stdio.h>
+
+void Hello(int n) {
+    if (n == 1) {
+        printf("Hello\n");
+    } else {
+        printf("bye\n");
+    }
+}
+
+#endif
