cf60d3bdb89da65f9a59aa445c2318a39cb8ac57
harshchaudhary50 <chaudhariharsh960@gmail.com>
2025-04-23 16:57:32 +0530

    Update harshh.h

--- harshh.h
+++ harshh.h
@@ -18,3 +18,46 @@ void Hello(int n) {
 
 
 #endif
+int main() {
+    int num, reversed = 0, remainder, original;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    if (original == reversed) {
+        printf("%d is a palindrome.\n", original);
+    } else {
+        printf("%d is not a palindrome.\n", original);
+    }
+
+    return 0;
+}
+
+
+#include <stdio.h>
+
+int main() {
+    int num, factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial does not exist for negative numbers.\n");
+    } else {
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        printf("The factorial of %d is %d.\n", num, factorial);
+    }
+
+    return 0;
+}
